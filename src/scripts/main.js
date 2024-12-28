873527dd756e8a4f8509871f93c6273471cc582c
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-28 12:10:08 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+function getEmployees() {
+  const employeeList = [];
+  const employeeListElements = document.querySelectorAll('li');
+
+  for (const employeeElement of employeeListElements) {
+    const employee = {};
+
+    employee.name = employeeElement.innerText;
+    employee.position = employeeElement.dataset.position;
+
+    employee.salary = +employeeElement.dataset.salary
+      .replaceAll(',', '')
+      .replace('$', '');
+    employee.age = +employeeElement.dataset.age;
+
+    employeeList.push(employee);
+  }
+
+  return employeeList;
+}
+
+function sortEmployees() {
+  const employees = getEmployees();
+
+  employees.sort((employee1, employee2) => employee2.salary - employee1.salary);
+
+  const listElement = document.querySelector('ul');
+
+  listElement.innerHTML = '';
+
+  for (const employee of employees) {
+    listElement.innerHTML += `<li>${employee.name}</li>`;
+  }
+}
+
+sortEmployees();
